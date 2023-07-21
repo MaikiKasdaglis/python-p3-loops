344b66bb23711712380a135a4e155205f2465760
Michael Kasdaglis <maikikasdaglis@gmail.com>
2023-07-21 17:08:03 -0600

    finishes lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,39 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
-
+   i = 10
+   while i >= 1:
+      print(i)
+      if i == 1: 
+         print('Happy New Year!')
+      i -= 1
+  
+  
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return_this = [num * num for num in int_list]
+    return return_this
+
+# def fizzbuzz():
+#    i = 1
+#    while i < 100:
+#       print(i)
+#       if i % 3 == 0 and i % 5 == 0:
+#          print("FizzBuzz")
+#       elif i % 3 == 0: 
+#          print('Fizz')
+#       elif i % 5 == 0:
+#          print('Buzz')
+#       i += 1
 
 def fizzbuzz():
-    # code goes here!
-    pass
+   i = 1
+   while i <= 100:
+      if i % 3 == 0 and i % 5 == 0:
+         print("FizzBuzz")
+      elif i % 3 == 0: 
+         print('Fizz')
+      elif i % 5 == 0:
+         print('Buzz')
+      else:
+         print(i)
+      i += 1
